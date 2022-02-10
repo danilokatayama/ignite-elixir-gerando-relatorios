89d88132c0ff62ace39a100f9a8cd5d871706c6d
Danilo Katayama <danilokatayama@gmail.com>
2022-02-10 13:26:39 -0300

    challenge finished

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,45 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  def build(), do: {:error, "Insira o nome de um arquivo"}
+
+  defp sum_values([name, hours, _day, month, year], %{
+         "all_hours" => all_hours,
+         "hours_per_month" => hours_per_month,
+         "hours_per_year" => hours_per_year
+       }) do
+    all_hours = merge(all_hours, %{name => hours})
+
+    hours_per_month =
+      Map.merge(hours_per_month, %{name => %{month => hours}}, fn _k, v1, v2 -> merge(v1, v2) end)
+
+    hours_per_year =
+      Map.merge(hours_per_year, %{name => %{year => hours}}, fn _k, v1, v2 -> merge(v1, v2) end)
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp merge(map1, map2), do: Map.merge(map1, map2, fn _k, v1, v2 -> v1 + v2 end)
+
+  defp report_acc() do
+    build_report(%{}, %{}, %{})
+  end
+
+  defp build_report(
+         all_hours,
+         hours_per_month,
+         hours_per_year
+       ) do
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
   end
 end
